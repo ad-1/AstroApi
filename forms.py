028c1fdcade60a1c5f4d002b930b35e8ec8cd897
AndyJ <andrewdaviesul@gmail.com>
2020-04-06 22:49:44 +0100

    removed input fields, added json textarea

--- forms.py
+++ forms.py
@@ -1,10 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 
 class CelestialDataForm(FlaskForm):
-    name = StringField('Object', validators=[DataRequired(), Length(min=2, max=20)])
-    radius = StringField('Radius (km)', validators=[DataRequired(), ])
-    mass = StringField('Mass (kg)', validators=[DataRequired()])
+    body = TextAreaField('JSON Data', validators=[DataRequired()])
     submit = SubmitField('Submit Data')
