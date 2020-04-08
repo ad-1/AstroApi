84c6cfa9669f8fb169de5d9b54860794e59f68ab
AndyJ <andrewdaviesul@gmail.com>
2020-04-08 21:06:50 +0100

    updating db

--- init_db.py
+++ init_db.py
@@ -12,15 +12,30 @@ data = {
     "gravity": 9.81,
     "mass": 5.972e+24,
     "radius": 6378,
-    "sideread_rotation_period": 23.9345,
+    "sidereal_rotation_period": 23.9345,
     "inclination_of_equator_to_orbit_plane": 177.4,
     "semimajor_axis": 149.6e6,
     "eccentricity": 0.0167,
     "inclination_orbit_to_ecliptic_plane": 0,
     "orbit_sidereal_period" : 8766.144,
-    "gravitation_parameter": 398600,
+    "gravitational_parameter": 398600,
     "sphere_of_influence": 925000,
 }
+
+# data = {
+#     "name": "Mercury",
+#     "gravity": 3.7,
+#     "mass": 330.2e21,
+#     "radius": 2240,
+#     "sidereal_rotation_period": 1407.6,
+#     "inclination_of_equator_to_orbit_plane": 0.01,
+#     "semimajor_axis": 57.91e6,
+#     "eccentricity": 0.2056,
+#     "inclination_orbit_to_ecliptic_plane": 7.0,
+#     "orbit_sidereal_period" : 2111.28,
+#     "gravitational_parameter": 22030,
+#     "sphere_of_influence": 112000
+# }
 
 body = Body(data)
 
